35472eb8f7a8ff6169de25875bd9cccdbfaf940d
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-24 10:17:34 -0500

    Set up initial App files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//**1:ReactDom is setting up the virtual dom, we have 2 doms now, the shallow copy and the actual dom in the browser
+//The virtual dom spins up first then mounts it to the actual dom, when changes happen the diffing algorithm looks at 
+//the changes and finds the most efficinet way to make those changes in the actual dom.
+//This is saying render the app component to this mounting point, this is grabbing the root id from index.html
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
