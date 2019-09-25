6c000bfa8604ddf0027e835ea9ad1980c2729f91
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-25 10:02:00 -0500

    Skeleton CSS added to project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav'
+import Footer from './Footer'
 
 function App() {
   return (
-    <div className="App">
+    //Nav is inside the div bc react works from inside out, everything needs to 
+    //be wrapped in some way  If it is outside you will get an error 
+    <div className="App container">
       <Nav/>
       <h1>Confetti Blog!</h1>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return ( <div className = "footer" > Built by MetaHeather with &#9829;</div>); 
+    }
+}
+
+export default Footer;
